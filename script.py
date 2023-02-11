4f07ff2b70f3adbc8695c257055f9a7932c60b71
Kumar Ramendra <ram@Ram.local>
2023-02-11 20:11:48 +0530

    removed the product and divide

--- script.py
+++ script.py
@@ -1,12 +1,7 @@
 def add(x, y):
     return x + y
 
-def product( a, b):
-    return a * b
-
-def divide(c, d):
-    return c /d
-
-result = add(2,3) + product(4,5) + divide(2,3)
+result = add(2,3)
 
 print("The result is :",result)
+print("THis is the end of the code")
